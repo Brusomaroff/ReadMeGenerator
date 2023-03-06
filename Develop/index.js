108df42b64153140e641923887533a2589480ba4
Brusomaroff <brusomaroff@gmail.com>
2023-03-05 20:38:29 -0500

    just need reamde

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,83 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const genterateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project',
+        validate: (value) => { if (value) { return true } else { return `Please name you project.` } },
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description about your project',
+        validate: (value) => { if (value) { return true } else { return `Please enter a description of your project.` } },
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions for your projects',
+        validate: (value) => { if (value) { return true } else { return `Please give instructions on how to install your project.` } },
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information for your project?',
+        validate: (value) => { if (value) { return true } else { return `Please provide the usage information of the project.` } },
+    },
+    {
+        type:'input',
+        name:'contributions',
+        message:'What are the contributing guidlines',
+    },
+    {
+        type:'input',
+        name:'test',
+        message:'What are the testing instructions for your project',
+    },
+    {
+        type:'checkbox',
+        name:'badge',
+        message:'What are you asking for your project?',
+        choices: ["MIT", "Apache", "GPL", "None"],
+    },
+    {
+        type:'input',
+        name:'username',
+        message:'What is your Github username?',
+        choices: [],
+        validate: (value) => { if (value) { return true } else { return `Please enter your Github username.` } },
+    },
+    {
+        type:'input',
+        name:'email',
+        message:'What is your email',
+        choices: [],
+        validate: (value) => { if (value) { return true } else { return `Please enter a valid email.` } },
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    data = genterateMarkdown(data)
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err): console.log('Congratulations you sucessfully created a Readme.md file')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    filename = "Readme.md"
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        console.log(data, "data")
+        writeToFile(filename, data)
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,73 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let yourLicense = ''
+  if(license === 'MIT'){
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (license === 'Apache'){
+    yourLicense = `![License: Apache](https://img.shields.io/badge/License-Apache-blue.svg)`
+  } else if (license === 'GPL'){
+    yourLicense = `![License: GPL](https://img.shields.io/badge/License-GPL-red.svg)`
+  }
+  return yourLicense;
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let yourLink = ''
+  if (license=== 'Mit' ||  license === "Apache" || license === "GPL" ) {
+    yourLink = `-[License](#license)`
+  }
+ 
+  return yourLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `# License 
+    - User is using this ${license} license`
+  }
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} ${renderLicenseBadge(data.badge)}
+
+  # Description 
+  -${data.description}
+  
+
+
+  # Table of Contents 
+  -Installation 
+  -Contributing
+  -Test
+  -Questions
+  ${renderLicenseLink(data.badge)}
+
+  # Installation
+  -${data.installation}
+
+  # Usage 
+  -${data.usage}
+
+  # Contributions
+  -${data.contributions}
+
+  # Test
+  -${data.test}
+
+
+  # Questions
+  -${data.username}
+  -${data.email}
+
+  ${renderLicenseSection(data.badge)}
 
 `;
 }
